289d6933ca27a7facbcc3ae0ede60b468c2a5bde
Henry Avery <gocha.berulava@gmail.com>
2021-11-18 09:28:05 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log ('arr'+" - object");
+console.log (4+ parseInt("27"));
+let incr = 10,
+    decr = 10;
+incr++;
+console.log (incr);
+let isChecked = false,
+    isClose = !true;
+    console.log ( isChecked || !isClose);
